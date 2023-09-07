bb2fa06bda3d290c6101139c68538c07d40e08dc
Birendra Dhami <88662210+birendradhami@users.noreply.github.com>
2023-09-08 00:49:01 +0545

    Update cart.js

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -80,7 +80,7 @@ let buttonTag = document.createElement('button')
 buttonDiv.appendChild(buttonTag)
 
 let buttonLink = document.createElement('a')
-buttonLink.href = '/orderPlaced.html?'
+buttonLink.href = 'orderPlaced.html?'
 buttonTag.appendChild(buttonLink)
 
 buttonText = document.createTextNode('Place Order')
